14969e3d9c313dc1c1c4eae00abc826b9deabc69
shehabeldeenibrahim <shehab_eldiin@hotmail.com>
2021-05-28 14:51:32 +0200

    added tests to flatlist scrolling

--- src/tests/MoviesList.test.js
+++ src/tests/MoviesList.test.js
@@ -2,16 +2,13 @@ import React from "react";
 import Adapter from "enzyme-adapter-react-16";
 import { shallow, configure } from "enzyme";
 import MoviesList from "../Components/MoviesList";
-import { render } from "@testing-library/react-native";
+import { fireEvent, render } from "@testing-library/react-native";
 import { data } from "./MoviesDataMock";
 
 configure({ adapter: new Adapter() });
 
 describe("Movies List", () => {
-  it("renders correctly", () => {
-    shallow(<MoviesList />);
-  });
-
+  /* Test Rendering of props in DOM */
   it("Movies List Rendered Correctly", async () => {
     const retrieveMore = jest.fn();
     const { getByTestId, getByText, UNSAFE_getByType } = render(
@@ -35,45 +32,42 @@ describe("Movies List", () => {
     expect(views).toBeDefined();
     expect(views.children.length).toEqual(data.length);
   });
-  // it("Flatlist Unit Test", () => {
-  //   const onEndReached = jest.fn();
-  //   const { getByTestId } = render(
-  //     <FlatList
-  //       data={Array.from({ length: 10 }, (_, key) => ({ key: `${key}` }))}
-  //       renderItem={() => (
-  //         <MovieCard
-  //           title={"Test"}
-  //           image_uri={"https://picsum.photos/536/354"}
-  //           date={"22/55/5556"}
-  //           overview={"scsdcs"}
-  //           votes={456}
-  //           language={"en"}
-  //         />
-  //       )}
-  //       onEndReached={onEndReached}
-  //       onEndReachedThreshold={0.2}
-  //       testID="flat-list"
-  //     />
-  //   );
-  //   const eventData = {
-  //     nativeEvent: {
-  //       contentOffset: {
-  //         y: 500,
-  //       },
-  //       contentSize: {
-  //         // Dimensions of the scrollable content
-  //         height: 500,
-  //         width: 100,
-  //       },
-  //       layoutMeasurement: {
-  //         // Dimensions of the device
-  //         height: 100,
-  //         width: 100,
-  //       },
-  //     },
-  //   };
 
-  //   fireEvent.scroll(getByTestId("flat-list"), eventData);
-  //   expect(onEndReached).toHaveBeenCalled();
-  // });
+  /* Test Retrieve more function is called on scroll end
+   to retrieve more data (LAZY LOADING) */
+  it("Lazy Loading", () => {
+    /* Define mock function to retrieveMore */
+    const retrieveMore = jest.fn();
+
+    /* Render Component */
+    const { getByTestId, getByText, UNSAFE_getByType } = render(
+      <MoviesList
+        retrieveMore={retrieveMore}
+        data={data}
+        refreshing={false}
+        loading={false}
+      />
+    );
+    /* Scroll event configuration */
+    const eventData = {
+      nativeEvent: {
+        contentOffset: {
+          y: 500,
+        },
+        contentSize: {
+          // Dimensions of the scrollable content
+          height: 500,
+          width: 100,
+        },
+        layoutMeasurement: {
+          // Dimensions of the device
+          height: 100,
+          width: 100,
+        },
+      },
+    };
+    /* Fire scroll event and check if retieveMore has been called */
+    fireEvent.scroll(getByTestId("list"), eventData);
+    expect(retrieveMore).toHaveBeenCalled();
+  });
 });
