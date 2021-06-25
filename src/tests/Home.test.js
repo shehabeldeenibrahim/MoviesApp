9ddbab1f7f7d5bfbf8dc9efe2faca3d723715b88
shehabeldeenibrahim <shehab_eldiin@hotmail.com>
2021-06-25 23:51:20 +0200

    added test for API integration in Home component + UI test

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -2,23 +2,47 @@ import React from "react";
 import Adapter from "enzyme-adapter-react-16";
 import { configure } from "enzyme";
 import Home from "../Screens/Home";
-import { fireEvent, render } from "@testing-library/react-native";
-import { data } from "./MoviesDataMock";
+import { render, waitFor, cleanup } from "@testing-library/react-native";
+import { data } from "../tests/MoviesDataMock";
 
+// intercept axios calls
+jest.mock("axios");
+
+// enzyme configuration
 configure({ adapter: new Adapter() });
+
+// do clean up after each test
+afterEach(cleanup);
 
 describe("Home Screen", () => {
   /* Test Rendering of props in DOM */
-  it("Home Screen Rendered Correctly", async () => {
-    const { getByTestId, getByText, UNSAFE_getByType, debug } = render(
-      <Home />
+  it("fetches data and displays list", async () => {
+    function returnPromise() {
+      return new Promise(function (resolve, reject) {
+        resolve(data);
+      });
+    }
+    function getMoviesMock() {
+      return returnPromise();
+    }
+    const { findByTestId, getByTestId, getByText, rerender, debug } = render(
+      <Home getMovies={getMoviesMock} />
     );
 
     /* Check that NoData Component is rendered
-      it will always be rendered as Home does not take
-      data as props so we cannot simulate the behavior
+     when the component first renders before api call is done
+     and check that it is rendered correctly with given text
     */
     const dataRendered = getByText("No data available");
     expect(dataRendered).toBeDefined();
+
+    /* Check if list of movies are rendered after
+      api call is done, and state updates
+      by passing mocked fn simulating API call
+    */
+    rerender(<Home getMovies={getMoviesMock} />);
+    const resolvedSpan = await waitFor(() => findByTestId("list"), {
+      timeout: 4000,
+    });
   });
 });
